36a8801afa2bf7b167358f85058ff7db3db6d1e1
michman <arlitin251@gmail.com>
2022-09-19 16:05:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mychat/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.mychat
+
+import android.graphics.drawable.BitmapDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mychat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.ktx.database
+import com.squareup.picasso.Picasso
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var auth:FirebaseAuth
+    lateinit var adapter: UserAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        setUpActionBar()
+        val database = Firebase.database
+        val myRef = database.getReference("messages")
+
+        binding.bSend.setOnClickListener{
+            myRef.child(myRef.push().key?: "blabla").setValue(User(auth.currentUser?.displayName,binding.edMessage.text.toString()))
+        }
+        onChangeListener(myRef)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        adapter = UserAdapter()
+        rcView.layoutManager = LinearLayoutManager(this@MainActivity)
+        rcView.adapter = adapter
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.sign_out){
+            auth.signOut()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun onChangeListener(dRef: DatabaseReference){
+        dRef.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list = ArrayList<User>()
+               for(s in snapshot.children){
+                  val user = s.getValue(User::class.java)
+                   if(user != null) list.add(user)
+               }
+                adapter.submitList(list)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+    }
+
+    private fun setUpActionBar(){
+        val ab = supportActionBar
+        Thread{
+            val bMap = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val dIcon = BitmapDrawable(resources, bMap)
+            runOnUiThread{
+                ab?.setDisplayHomeAsUpEnabled(true)
+                ab?.setHomeAsUpIndicator(dIcon)
+                ab?.title = auth.currentUser?.displayName
+            }
+        }.start()
+    }
+}
